2aa92c7388cedb4a9a466d2aa30827aaa99df08d
Junseo Bae <ferrater1013@gmail.com>
2026-07-21 22:59:41 +0900

    chore: core-db 모듈에 core-enum 의존성 추가

--- storage/db-core/build.gradle.kts
+++ storage/db-core/build.gradle.kts
@@ -5,6 +5,8 @@ allOpen {
 }
 
 dependencies {
+    api(project(":core:core-enum"))
+
     api("org.springframework.boot:spring-boot-starter-data-jpa")
     runtimeOnly("com.mysql:mysql-connector-j")
     runtimeOnly("com.h2database:h2")
