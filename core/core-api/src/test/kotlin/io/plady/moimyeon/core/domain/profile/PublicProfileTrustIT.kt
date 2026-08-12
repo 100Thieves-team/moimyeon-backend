cc4581dd5a69a3ba8aa6dd259099a61a559a2fe9
Junseo Bae <ferrater1013@gmail.com>
2026-08-12 20:49:18 +0900

    test(profile): 공개 프로필 요구사항 스켈레톤 추가

---
+++ core/core-api/src/test/kotlin/io/plady/moimyeon/core/domain/profile/PublicProfileTrustIT.kt
@@ -0,0 +1,26 @@
+package io.plady.moimyeon.core.domain.profile
+
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
+
+class PublicProfileTrustIT {
+    @Disabled("MOI-378 구현 예정")
+    @Test
+    fun `완료되고 삭제되지 않은 룸의 출석과 활동만 집계한다`() {
+    }
+
+    @Disabled("MOI-378 구현 예정")
+    @Test
+    fun `최근 출석은 시작 시각과 룸 식별자 역순으로 불참을 포함해 3건 반환한다`() {
+    }
+
+    @Disabled("MOI-378 구현 예정")
+    @Test
+    fun `누적 불참은 완료 룸의 모든 불참을 집계한다`() {
+    }
+
+    @Disabled("MOI-378 구현 예정")
+    @Test
+    fun `대표 태그는 공개 시각과 숨김 삭제 상태를 반영해 상위 3개를 반환한다`() {
+    }
+}
