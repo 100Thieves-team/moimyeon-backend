d131d7dd8363c5f7edf31408db622cfcf5457516
YUJE LEE <dbwp031@gmail.com>
2026-09-24 16:41:32 +0900

    feat(notification): 룸 생명주기 이벤트 소비 호환 추가

--- core/core-enum/src/main/kotlin/io/plady/moimyeon/core/enums/EventType.kt
+++ core/core-enum/src/main/kotlin/io/plady/moimyeon/core/enums/EventType.kt
@@ -6,4 +6,16 @@ enum class EventType(
     ROOM_APPLICATION_ACCEPTED(
         notificationChannels = setOf(NotificationChannel.WEB_PUSH, NotificationChannel.EMAIL),
     ),
+    ROOM_CONFIRMED(
+        notificationChannels = setOf(NotificationChannel.WEB_PUSH, NotificationChannel.EMAIL),
+    ),
+    ROOM_COMPLETED(
+        notificationChannels = setOf(NotificationChannel.WEB_PUSH, NotificationChannel.EMAIL),
+    ),
+    ROOM_CANCELED(
+        notificationChannels = setOf(NotificationChannel.WEB_PUSH, NotificationChannel.EMAIL),
+    ),
+    ROOM_REVIEW_REQUESTED(
+        notificationChannels = setOf(NotificationChannel.WEB_PUSH, NotificationChannel.EMAIL),
+    ),
 }
